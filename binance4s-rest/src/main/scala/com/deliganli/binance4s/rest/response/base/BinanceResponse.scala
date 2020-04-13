e9f71ece83e06d07df9435b96bb9f5e86326acad
Yoann Laurent <laurent.yoann@gmail.com>
2020-04-13 13:16:57 +0900

    Fix BinanceResponse usedWeight / orderCount

--- binance4s-rest/src/main/scala/com/deliganli/binance4s/rest/response/base/BinanceResponse.scala
+++ binance4s-rest/src/main/scala/com/deliganli/binance4s/rest/response/base/BinanceResponse.scala
@@ -9,8 +9,8 @@ import org.http4s.util.CaseInsensitiveString
 import org.http4s.{Headers, Response, Status}
 
 case class BinanceResponse[T](status: Status, body: T, headers: Headers) {
-  def usedWeight: Int = headers.get(CaseInsensitiveString("x-mbx-used-weight")).map(_.renderString.toInt).getOrElse(0)
-  def orderCount: Int = headers.get(CaseInsensitiveString("x-mbx-order-count")).map(_.renderString.toInt).getOrElse(0)
+  def usedWeight: Int = headers.get(CaseInsensitiveString("x-mbx-used-weight")).map(_.value.toInt).getOrElse(0)
+  def orderCount: Int = headers.get(CaseInsensitiveString("x-mbx-order-count")).map(_.value.toInt).getOrElse(0)
 }
 
 object BinanceResponse {
