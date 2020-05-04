de00e2b540a66c8a7cbaa4907eefdb9b17fc44ea
Yoann Laurent <laurent.yoann@gmail.com>
2020-05-04 19:47:22 +0900

    Fix exchangeInfo + add new API fields

---
+++ binance4s-rest/src/main/scala/com/deliganli/binance4s/rest/response/market/Permissions.scala
@@ -0,0 +1,12 @@
+package com.deliganli.binance4s.rest.response.market
+
+import enumeratum._
+
+sealed abstract class Permissions extends EnumEntry
+
+object Permissions extends Enum[Permissions] with CirceEnum[Permissions] {
+  case object SPOT   extends Permissions
+  case object MARGIN extends Permissions
+
+  val values = findValues
+}
--- binance4s-rest/src/test/scala/com/deliganli/binance4s/rest/client/PublicClientTest.scala
+++ binance4s-rest/src/test/scala/com/deliganli/binance4s/rest/client/PublicClientTest.scala
@@ -6,6 +6,7 @@ import com.deliganli.binance4s.common.http.JDKHttpClient
 import com.deliganli.binance4s.rest.BinanceRestClient
 import com.deliganli.binance4s.rest.framework.IntegrationTest
 import org.http4s.client.jdkhttpclient.JdkHttpClient
+import com.deliganli.binance4s.rest.response.base.BinanceResponse
 
 class PublicClientTest extends IntegrationTest {
 
@@ -14,56 +15,56 @@ class PublicClientTest extends IntegrationTest {
   )
 
   "ping" should "succeed" in {
-    noException should be thrownBy rest.general.ping.unsafeRunSync()
+    rest.general.ping.unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "time" should "succeed" in {
-    noException should be thrownBy rest.general.time.unsafeRunSync()
+    rest.general.time.unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "exchange info" should "succeed" in {
-    noException should be thrownBy rest.general.exchangeInfo.unsafeRunSync()
+    rest.general.exchangeInfo.unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   val symbol = "BTCUSDT"
 
   "avg price" should "succeed" in {
-    noException should be thrownBy rest.market.avgPrice(symbol).unsafeRunSync()
+    rest.market.avgPrice(symbol).unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "depth" should "succeed" in {
-    noException should be thrownBy rest.market.depth(symbol, DepthLimit.Five).unsafeRunSync()
+    rest.market.depth(symbol, DepthLimit.Five).unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "klines" should "succeed" in {
-    noException should be thrownBy rest.market.klines(symbol, KlineInterval.Daily, 5).unsafeRunSync()
+    rest.market.klines(symbol, KlineInterval.Daily, 5).unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "book ticker for single currency" should "succeed" in {
-    noException should be thrownBy rest.market.ticker.bookAll(symbol).unsafeRunSync()
+    rest.market.ticker.bookAll(symbol).unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "book ticker for all" should "succeed" in {
-    noException should be thrownBy rest.market.ticker.book.unsafeRunSync()
+    rest.market.ticker.book.unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "daily ticker for single currency" should "succeed" in {
-    noException should be thrownBy rest.market.ticker.daily(symbol).unsafeRunSync()
+    rest.market.ticker.daily(symbol).unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "daily ticker for all" should "succeed" in {
-    noException should be thrownBy rest.market.ticker.dailyAll.unsafeRunSync()
+    rest.market.ticker.dailyAll.unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "price ticker for single currency" should "succeed" in {
-    noException should be thrownBy rest.market.ticker.price(symbol).unsafeRunSync()
+    rest.market.ticker.price(symbol).unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "price ticker for all" should "succeed" in {
-    noException should be thrownBy rest.market.ticker.priceAll.unsafeRunSync()
+    rest.market.ticker.priceAll.unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 
   "recent trades" should "succeed" in {
-    noException should be thrownBy rest.market.trade.recent(symbol, 1).unsafeRunSync()
+    rest.market.trade.recent(symbol, 1).unsafeRunSync() should matchPattern { case BinanceResponse(_, Right(_), _) => }
   }
 }
