d262e045a1e02cbc1903504bc1bd514ae959bcf9
Yoann Laurent <laurent.yoann@gmail.com>
2020-05-09 19:54:19 +0900

    Add LEVERAGED permissions

--- binance4s-rest/src/main/scala/com/deliganli/binance4s/rest/response/market/Permissions.scala
+++ binance4s-rest/src/main/scala/com/deliganli/binance4s/rest/response/market/Permissions.scala
@@ -5,8 +5,9 @@ import enumeratum._
 sealed abstract class Permissions extends EnumEntry
 
 object Permissions extends Enum[Permissions] with CirceEnum[Permissions] {
-  case object SPOT   extends Permissions
-  case object MARGIN extends Permissions
+  case object SPOT      extends Permissions
+  case object MARGIN    extends Permissions
+  case object LEVERAGED extends Permissions
 
   val values = findValues
 }
